e631d6198739552e2b160920f163bfd7b0005e85
Ridwan6947 <81156501+Ridwan6947@users.noreply.github.com>
2022-06-16 23:40:41 +0530

    Add files via upload

---
+++ rotate_by_90.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int arr[][] = new int[n][n];
+        for (int i = 0 ;  i < arr.length ; i++){
+            for (int j = 0 ; j < arr[0].length ; j++){
+                arr[i][j] = scan.nextInt();
+            }
+        }
+
+        //Transpose of a matrix
+
+        for (int i = 0 ; i < arr.length ; i++){
+            for (int j = i ; j < arr[0].length ; j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+
+        //Reverse of transposed Matrix
+
+        for(int i = 0 ; i < arr.length ; i++){
+            int li = 0;
+            int ri = arr[i].length-1;
+            while(li < ri){
+                int temp = arr[i][li];
+                arr[i][li] = arr[i][ri];
+                arr[i][ri] = temp;
+                li++;
+                ri--;
+            }
+
+        }
+        display(arr);
+    }
+    public static void display(int arr[][]){
+        for (int i = 0 ; i < arr.length ; i++){
+            for (int j = 0 ; j < arr[0].length ; j++){
+                System.out.println(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
